75d477241a3ceaf3842de72ea09295ee6a9fbbc0
ivanIRP <ivatar1066@gmail.com>
2026-02-20 12:26:44 -0600

    ajuste del archivo para subir al host

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,37 @@
+import js from "@eslint/js";
+import globals from "globals";
+
 export default [
+  js.configs.recommended,
   {
     languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.browser,
-        renderPeliculas: "readonly", 
+        renderPeliculas: "readonly",
         renderClientes: "readonly",
-        renderUsuarios: "readonly"
-      }
+        renderUsuarios: "readonly",
+        renderAutoClaves: "readonly",
+        showSection: "readonly",
+        doLogin: "readonly",
+        doLogout: "readonly",
+        registrarPelicula: "readonly",
+        registrarCliente: "readonly",
+        registrarUsuario: "readonly",
+        togglePeli: "readonly",
+        showToast: "readonly",
+        genClave: "readonly"
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      
+      "no-undef": "warn",
+      
+      "no-console": "off",
+
+      "prefer-const": "off"
     }
   }
 ];
